1c18ba81935c89fbd1b9c20e58cb147db7d84baf
daniantu <d.asenjo.c@gmail.com>
2019-03-03 18:54:27 -0300

    Verifica archivos de Carptas

--- mdLinks.js
+++ mdLinks.js
@@ -1,32 +1,33 @@
+require('colors');
 const fs = require('fs');
 const path = require('path');
+const fetch = require('node-fetch');
 const markdownLinkExtractor = require('markdown-link-extractor');
 
 module.exports = {
-  mdlinks: (inputPath, option) => {
+  linkInspector: (inputPath) => {
     function reviewInputPath(pathToReview) { // Función que revisa links dentro del archivo MD
       const extractMd = fs.readFileSync(pathToReview).toString(); // Lee el archivo
       const links = markdownLinkExtractor(extractMd); // Extrae los links
       links.forEach((link) => {
         console.log(link);
+        // NODE FETCH y usar Promesas para verificar el estado de los links
+        // Revisar Status en documentación de NODE Fetch
       });
     }
-  },REA
-  linkInspector: (inputPath) => {
-    // TRANSFORMAR A PROMESA
-    // VERIFICAR QUE TEXTO INGRESADO SEA VALIDO
-    // VERIFICAR TEXTO VACÍO
-    // ===========================================================================
-    if (path.isAbsolute(inputPath)) { // Verifico si "ruta" es directorio o archivo
-      // Es directorio
-      console.log(inputPath);
+    if (path.extname(inputPath) === '.md') {
+      reviewInputPath(inputPath);
+    } else if (path.isAbsolute(inputPath)) {
+      console.log('Es directorio');
+      // Llamar recursivamente a la función para que llame de nuevo si encuentra carpetas dentro de carpetas
+    } else if (inputPath === '') {
+      console.log('No ingresaste nada.'.red);
     } else {
-      // Es archivo
-      console.log('Es archivo');
-      
+      console.log('Algo anda mal. Ingresa de nuevo.'.red);
     }
   },
 };
+
 
 // LO QUE DEBE RETORNAR LA FUNCIÓN
 // ===========================================================================
